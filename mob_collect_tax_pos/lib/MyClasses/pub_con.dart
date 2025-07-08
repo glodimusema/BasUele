716395741149a32bbd34bda1ffce33fae7017a9c
glodimusema <glodimaley@isig.ac.cd>
2025-07-08 19:42:53 +0200

    mobile

---
+++ mob_collect_tax_pos/lib/MyClasses/pub_con.dart
@@ -0,0 +1,23 @@
+//import 'package:flutter/material.dart';
+
+class PubCon {
+  static const url_domaine = "www.ffn-tshopo-bas-uele.site";
+  static const cheminApi = "https://ffn-tshopo-bas-uele.site/api/";
+//static const colorPrim=Color(0xff49A2B6);
+  static String codeSecret = "-1",
+      DesignationEntreprise = "",
+      PrixEntreprise = "",
+      DesignationAnnee = "",
+      DesignationMois = "",
+      CategorieEntreprise = "";
+  static int id_agent = 0,
+      refMvt = 1,
+      refEntreprise = -1,
+      refAnnee = -1,
+      refMois = -1;
+  static String noms_author = "",
+      adresse_profil = "",
+      telephone_profil = "000",
+      mail = "";
+//ffn-tshopo-bas-uele
+}
